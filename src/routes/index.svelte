a3fb7efa78dd5ec8bc064b59df4be1374045567b
Nofi-nof <takenoko2402@gmail.com>
2021-09-07 19:17:59 +0900

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,6 @@
+<script>
+	let message = 'POKEMON';
+</script>
+
+<h1 class="text-center text-yellow-200 bg-yellow-600 text-5xl">{message}</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
