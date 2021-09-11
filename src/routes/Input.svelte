c81d2205e9555f57f82c4f09e75eca93d213275c
Nofi-nof <takenoko2402@gmail.com>
2021-09-11 20:29:47 +0900

    Recreate file →pokemon

---
+++ src/routes/Input.svelte
@@ -0,0 +1,5 @@
+<script>
+	export let searchWord;
+</script>
+
+<input bind:value={searchWord} class="rounded-l-xl shadow-2xl p-1" />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,49 @@
 <script>
-	let message = 'POKEMON';
+	import Pokemon from './Pokemon.svelte';
+	import Input from './Input.svelte';
+
+	let searchWord = '';
+
+	const pokemons = [
+		{
+			name: 'フシギダネ',
+			type: 'くさ',
+			hp: 45,
+			atk: 49,
+			def: 49,
+			sAtk: 65,
+			sDef: 65,
+			spd: 45
+		},
+		{
+			name: 'ヒトカゲ',
+			type: 'ほのお',
+			hp: 39,
+			atk: 52,
+			def: 43,
+			sAtk: 60,
+			sDef: 50,
+			spd: 65
+		},
+		{
+			name: 'ゼニガメ',
+			type: 'みず',
+			hp: 44,
+			atk: 48,
+			def: 65,
+			sAtk: 50,
+			sDef: 64,
+			spd: 43
+		}
+	];
+
+	$: filterPokemons = () => pokemons.filter((pokemon) => pokemon.name === searchWord);
 </script>
 
-<h1 class="text-center text-yellow-200 bg-yellow-600 text-5xl">{message}</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Input bind:searchWord />
+
+<main>
+	{#each filterPokemons() as p}
+		<Pokemon pokemon={p} />
+	{/each}
+</main>
